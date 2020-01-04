e5b14f2c320682904f840e40d822eed3142e681b
raijin0704 <raijin.1059@gmail.com>
2020-01-04 15:14:59 +0900

    fix estimator_errors & DecisionTreeClassifier

--- MultipleSourceTrAdaBoost.py
+++ MultipleSourceTrAdaBoost.py
@@ -7,14 +7,14 @@ based on algorithm 3 in paper "Boosting for transfer learning with multiple sour
 
 import numpy as np
 import copy
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import KFold
 
 
 class MultipleSourceTrAdaBoost:
     def __init__(self,
-                 base_estimator = DecisionTreeRegressor(max_depth=4),
+                 base_estimator = DecisionTreeClassifier(max_depth=4),
                  sample_size = None,
                  n_estimators = 50,
                  learning_rate = 1.,
@@ -56,7 +56,7 @@ class MultipleSourceTrAdaBoost:
         # Clear any previous fit results
         self.estimators_ = []
         self.estimator_weights_ = np.zeros(self.n_estimators, dtype=np.float64)
-        self.estimator_errors_ = np.ones(self.n_estimators, dtype=np.float64)
+        estimator_errors_ = np.ones(self.n_estimators, dtype=np.float64)
         self.accept_source = np.full(self.n_estimators, -1)
 
         for iboost in range(self.n_estimators): # this for loop is sequential and does not support parallel(revison is needed if making parallel)
@@ -72,9 +72,9 @@ class MultipleSourceTrAdaBoost:
             before_sum_size = 0
             for idx, isource in enumerate(self.sample_size[:-1]):
                 # 対象のsource以外のsourceデータを除く
-                X_isource = np.concatenate([X[before_sum_size:before_sum_size+isource], X[-sample_size[-1]:]])
-                y_isource = np.concatenate([y[before_sum_size:before_sum_size+isource], y[-sample_size[-1]:]])
-                sample_weight_isource = np.concatenate([sample_weight[before_sum_size:before_sum_size+isource], sample_weight[-sample_size[-1]:]])
+                X_isource = np.concatenate([X[before_sum_size:before_sum_size+isource], X[-self.sample_size[-1]:]])
+                y_isource = np.concatenate([y[before_sum_size:before_sum_size+isource], y[-self.sample_size[-1]:]])
+                sample_weight_isource = np.concatenate([sample_weight[before_sum_size:before_sum_size+isource], sample_weight[-self.sample_size[-1]:]])
                 # 弱学習器の作成と予測誤差の計算
                 eachstep_estimator_error[idx], error_vect, weak_estimator = self._MultipleSourceTrAdaBoost(
                         iboost,
@@ -88,25 +88,25 @@ class MultipleSourceTrAdaBoost:
             # 最も誤差の小さいsourceからの弱学習器を採用する
             self.accept_source[iboost] = np.argmin(eachstep_estimator_error)
             self.estimators_.append(weak_estimators[np.argmin(eachstep_estimator_error)])
-            self.estimator_errors_[iboost] = eachstep_estimator_error[np.argmin(eachstep_estimator_error)]
+            estimator_errors_[iboost] = eachstep_estimator_error[np.argmin(eachstep_estimator_error)]
             
             # 
-            if self.estimator_errors_[iboost] <= 0:
+            if estimator_errors_[iboost] <= 0:
                 # Stop if fit is perfect
                 print("fit is prefect")
                 break
 
             # 予測誤差が0.5以上ならその弱学習器は不採用
-            elif self.estimator_errors_[iboost] >= 0.5:
+            elif estimator_errors_[iboost] >= 0.5:
                 # Discard current estimator only if it isn't the only one
                 if len(self.estimators_) > 1:
                     self.estimators_.pop(-1)
-                    # self.estimator_errors_[iboost] = 
+                    # estimator_errors_[iboost] = -1
 
             # 予測誤差が0<ε<0.5なら続行
             else:
                 # αを計算
-                alpha = 0.5 * np.log((1-self.estimator_errors_[iboost])/self.estimator_errors_[iboost])
+                alpha = 0.5 * np.log((1-estimator_errors_[iboost])/estimator_errors_[iboost])
 
             # sample_weight更新
             # target
@@ -137,7 +137,7 @@ class MultipleSourceTrAdaBoost:
             #     break
 
             # self.estimator_weights_[iboost] = estimator_weight
-            # self.estimator_errors_[iboost] = estimator_error
+            # estimator_errors_[iboost] = estimator_error
 
             # # Stop if error is zero
             # if estimator_error == 0:
@@ -152,6 +152,8 @@ class MultipleSourceTrAdaBoost:
             # if iboost < self.n_estimators - 1:
             #     # Normalize
             #     sample_weight /= sample_weight_sum
+        # ε>0.5で採用されなかったイテレーションの予測誤差を除く
+        self.estimator_errors_ = estimator_errors_[estimator_errors_<0.5]
         return self
 
 
@@ -263,7 +265,7 @@ if __name__ == "__main__":
     X = np.random.rand(sum(sample_size), 5)
     y1 = np.where(np.sum(X, axis=1)>2, 1,0)
     y = np.random.randint(0, 2, size=sum(sample_size))
-    clf = MultipleSourceTrAdaBoost(DecisionTreeRegressor(max_depth=6),
+    clf = MultipleSourceTrAdaBoost(DecisionTreeClassifier(max_depth=6),
                             n_estimators = 100, sample_size = sample_size, 
                             random_state = np.random.RandomState(1))
     clf.fit(X, y1)
